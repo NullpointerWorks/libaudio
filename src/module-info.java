d77f2d9e1d118e0ab1597dd5593aa69a2097ba87
Nullpointer <38044500+NullpointerWorks@users.noreply.github.com>
2021-09-29 15:35:43 +0200

    initial upload

---
+++ src/module-info.java
@@ -0,0 +1,12 @@
+/**
+ * This is free and unencumbered software released into the public domain.
+ * (http://unlicense.org/)
+ * Nullpointer Works (2021)
+ *
+ * @version 0.1.0
+ * @author Michiel Drost - Nullpointer Works
+ */
+module libnpw.audio 
+{
+	
+}
