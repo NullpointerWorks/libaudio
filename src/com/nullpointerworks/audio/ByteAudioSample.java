6d9ce3da9b90a224a842c149a9cbb05561049814
Nullpointer <38044500+NullpointerWorks@users.noreply.github.com>
2021-10-01 21:24:10 +0200

    update

--- src/com/nullpointerworks/audio/ByteAudioSample.java
+++ src/com/nullpointerworks/audio/ByteAudioSample.java
@@ -6,11 +6,10 @@ import java.io.IOException;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
-import javax.sound.sampled.FloatControl;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
-public class WaveAudioSample implements AudioSample 
+public class ByteAudioSample implements AudioSample 
 {
 	private final int STOPPED = 0;
 	private final int PLAYING = 1;
@@ -20,20 +19,19 @@ public class WaveAudioSample implements AudioSample
 	private long frame;
     private int status;
 	
-    public WaveAudioSample(final String path) 
-    		throws UnsupportedAudioFileException, 
-		    IOException, 
-		    LineUnavailableException
+    public ByteAudioSample(byte[] bytes)
     {
-    	File f = new File(path).getAbsoluteFile();
-    	AudioInputStream io = AudioSystem.getAudioInputStream(f);
-    	audioclip = AudioSystem.getClip();
-    	audioclip.open(io);
     	
-    	FloatControl volume = (FloatControl) audioclip.getControl(FloatControl.Type.MASTER_GAIN);
-        volume.setValue(0.0f);
     	
-    	audioclip.loop(0);
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
     	
     	status = STOPPED;
     }
@@ -41,7 +39,9 @@ public class WaveAudioSample implements AudioSample
 	@Override
 	public synchronized void play() 
 	{
-		audioclip.start();
+		
+		
+		
     	status = PLAYING;
 	}
 	
@@ -50,8 +50,9 @@ public class WaveAudioSample implements AudioSample
 	{
 		if (status == PLAYING)
 		{
-			frame = audioclip.getMicrosecondPosition();
-			audioclip.stop();
+			
+			
+			
 			status = PAUSED;
 		}
 	}
@@ -72,17 +73,21 @@ public class WaveAudioSample implements AudioSample
 	public synchronized void jump(long ms) 
 	{
 		
+		
+		status = PLAYING;
 	}
 	
 	@Override
 	public synchronized void restart() 
 	{
 		
+		status = PLAYING;
 	}
 	
 	@Override
 	public synchronized void stop() 
 	{
 		
+		status = STOPPED;
 	}
 }
--- src/test/AudioMain.java
+++ src/test/AudioMain.java
@@ -1,51 +1,71 @@
 package test;
 
+import java.io.File;
 import java.io.IOException;
 
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.DataLine.Info;
 import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.SourceDataLine;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.SwingUtilities;
 import javax.swing.JOptionPane;
 
 import com.nullpointerworks.audio.AudioSample;
-import com.nullpointerworks.audio.WaveAudioSample;
 
 public class AudioMain 
 {
 	public static void main(String[] args) 
 	{
-		new AudioMain();
-	}
-	
-	public AudioMain()
-	{
-		String audioPath = "D:\\Development\\Audio\\Workspace\\Skiffy\\skiffy.wav";
-		AudioSample skiffy = null;
-		
 		try 
 		{
-			skiffy = new WaveAudioSample(audioPath);
+			new AudioMain();
 		} 
-		catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) 
+		catch (IOException | UnsupportedAudioFileException | LineUnavailableException e) 
 		{
 			e.printStackTrace();
-			return;
 		}
+	}
+	
+	public AudioMain() throws IOException, UnsupportedAudioFileException, LineUnavailableException
+	{
+		// load file
+		String audioPath = "D:\\Development\\Audio\\Workspace\\Skiffy\\skiffy.wav";
+		File f = new File(audioPath).getAbsoluteFile();
+    	AudioInputStream io = AudioSystem.getAudioInputStream(f);
+    	
+    	// convert audio info
+    	AudioFormat baseFormat = io.getFormat();
+        AudioFormat audioFormat = new AudioFormat(	AudioFormat.Encoding.PCM_SIGNED, 
+									        		baseFormat.getSampleRate(), 
+									        		16, 
+									        		baseFormat.getChannels(), 
+									        		baseFormat.getChannels() * 2, 
+									        		baseFormat.getSampleRate(), 
+									        		false);
+        
+        // extract byte data
+        AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFormat, io);
+		byte[] audiodata = audioStream.readAllBytes();
 		
-		skiffy.play();
+		// get data line
+		Info info = new DataLine.Info(SourceDataLine.class, audioFormat);
+		SourceDataLine sdl = (SourceDataLine) AudioSystem.getLine(info);
+		
+		// play the sound
+		sdl.open(audioFormat);
+		sdl.start();
+		
+        sdl.write(audiodata, 0, audiodata.length);
+        
+        sdl.drain();
+        sdl.stop();
+        sdl.close();
 		
 		
-		
-		
-        // A GUI element to prevent the Clip's daemon Thread
-        // from terminating at the end of the main()
-		SwingUtilities.invokeLater(new Runnable() 
-		{
-            public void run()
-            {
-                JOptionPane.showMessageDialog(null, "Close to exit!");
-            }
-        });
 	}
 	
     
